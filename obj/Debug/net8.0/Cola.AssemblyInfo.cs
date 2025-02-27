1c66097e106ffd0ef2aa210f7004cebf8254a90f
Wei Qun Cao <weiqun.cao@siemens.com>
2025-02-27 15:48:01 +0800

    log

--- obj/Debug/net8.0/Cola.AssemblyInfo.cs
+++ obj/Debug/net8.0/Cola.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+634a77f46e977857e72418687e3db4ab7e94a33f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fe2ec3a06872131bd543934f80ab853e28171a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
